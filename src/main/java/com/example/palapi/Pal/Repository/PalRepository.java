f9c74e99e1326953f4ff1946cd45fdf3515b0f41
quickoabdul <quickoabdul@gmail.com>
2024-11-26 10:07:04 +0100

    push !

--- src/main/java/com/example/palapi/Pal/Repository/PalRepository.java
+++ src/main/java/com/example/palapi/Pal/Repository/PalRepository.java
@@ -4,12 +4,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.*;
 import com.example.palapi.Pal.Model.*;
 import java.util.*;
+
 @SuppressWarnings("unused")
 
 @Repository
 public interface PalRepository extends JpaRepository<Pal, Long> {
     Optional<Pal> findByName(String name);
+
     List<Pal> findByTypesContaining(String type);
+
     List<Pal> findTopNByOrderByRarityDesc(int n);
+
     List<Pal> findTopNByOrderByPriceDesc(int n);
 }
