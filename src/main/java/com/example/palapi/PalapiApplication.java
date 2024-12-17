b4c59928a735afe4ad49d1aa23f21ddcc1efdcd2
quickoabdul <quickoabdul@gmail.com>
2024-12-17 15:09:13 +0100

    add: Swagger

--- src/main/java/com/example/palapi/PalapiApplication.java
+++ src/main/java/com/example/palapi/PalapiApplication.java
@@ -2,16 +2,35 @@ package com.example.palapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+
+// import springfox.documentation.builders.PathSelectors;
+// import springfox.documentation.builders.RequestHandlerSelectors;
+// import springfox.documentation.spi.DocumentationType;
+// import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class PalapiApplication {
+
+    // @Configuration
+    // public class SpringFoxConfig {
+    // @Bean
+    // public Docket api() {
+    // return new Docket(DocumentationType.SWAGGER_2)
+    // .select()
+    // .apis(RequestHandlerSelectors.any())
+    // .paths(PathSelectors.any())
+    // .build();
+    // }
+    // }
 
     public static void main(String[] args) {
         SpringApplication.run(PalapiApplication.class, args);
         System.out.println("Hello, Palapi!");
         System.out.println("API : -> http://localhost:8080/api/v1/pals");
         System.out.println("Database access : -> http://localhost:8080/h2-console");
-        // System.out.println("Swagger : -> http://localhost:8080/swagger-ui/index.html");
+        System.out.println("Swagger : -> http://localhost:8080/swagger-ui/index.html");
     }
 
 }
